16fdd9c15d39372587df6a7939066823a99d782c
Oleg Chepelev <yorkblansh@gmail.com>
2022-10-31 20:41:13 +0300

    update vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 import replace from '@rollup/plugin-replace'
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  mode: 'development',
+  mode: 'production',
   base: '/',
   includeAssets: ['favicon.svg'],
   manifest: {
