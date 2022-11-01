edbadf3b8973a9f10325f02c3fdbf3aa46e23583
Oleg Chepelev <yorkblansh@gmail.com>
2022-11-01 15:25:32 +0300

    update src

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,43 +1,48 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router'
-import './Home.css'
+import React, { useState } from "react"
+import { useNavigate } from "react-router"
+import "./Home.css"
 
 function Home() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0)
 
-  const [name, setName] = useState('')
+	const [name, setName] = useState("")
 
-  const router = useNavigate()
+	const router = useNavigate()
 
-  // @ts-expect-error just ignore
-  const handleChange = (event) => {
-    setName(event.target.value || '')
-  }
+	// @ts-expect-error just ignore
+	const handleChange = (event) => {
+		setName(event.target.value || "")
+	}
 
-  // @ts-expect-error just ignore
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    if (name)
-      router(`/hi/${name}`)
-  }
+	// @ts-expect-error just ignore
+	const handleSubmit = (event) => {
+		event.preventDefault()
+		if (name) router(`/hi/${name}`)
+	}
 
-  return (
-    <div className="Home">
-      <p>
-        <button type="button" onClick={() => setCount(count => count + 1)}>
-            count is: {count}
-        </button>
-      </p>
-      <br />
-      <form onSubmit={handleSubmit}>
-        <input value={name} onChange={handleChange} type="text" aria-label="What's your name?" placeholder="What's your name?" />
-        <button type="submit">GO</button>
-      </form>
-      <br/>
-      <a href="/about">About</a>
-      <br/>
-    </div>
-  )
+	return (
+		<div className='Home'>
+			<p>
+				<button type='button' onClick={() => setCount((count) => count + 1)}>
+					count is: {count}
+				</button>
+			</p>
+			<br />
+			<form onSubmit={handleSubmit}>
+				<input
+					value={name}
+					onChange={handleChange}
+					type='text'
+					aria-label="What's your name?"
+					placeholder="What's your name?"
+				/>
+				<button type='submit'>GO</button>
+			</form>
+			<br />
+			<a href='/about'>About</a>
+			<br />
+		</div>
+	)
 }
 
 export default Home
--- src/pages/hi/[name].tsx
+++ src/pages/hi/[name].tsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import { useParams } from 'react-router'
+import React from "react"
+import { useParams } from "react-router"
 
 function Hi() {
-  // replaced dyanmicaly
-  const date = '__DATE__'
-  const params = useParams()
-  return (
-    <div>
-      <div><strong>/hi</strong> route, built at: { date }</div>
-      <p>Hi: { params.name }</p>
-      <br />
-      <a href="/">Go Home</a>
-    </div>
-  )
+	// replaced dyanmicaly
+	const date = "__DATE__"
+	const params = useParams()
+	return (
+		<div>
+			<div>
+				<strong>/hi</strong> route, built at: {date}
+			</div>
+			<p>Hi: {params.name}</p>
+			<br />
+			<a href="/">Go Home</a>
+		</div>
+	)
 }
 
 export default Hi
